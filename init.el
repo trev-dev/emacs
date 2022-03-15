375c43d52567791e43c3ffe4885baf5f0d0c6bbf
Trevor Richards <trev@trevdev.ca>
2022-03-15 15:51:31 -0700

    move quelpa to init to make usage easier

--- init.el
+++ init.el
@@ -11,15 +11,26 @@
 
 ;;; Code:
 
-;; Load config.el if it exists. If it does not, tangle the configuration file
-;; and prompt the user to restart. By using this method, we remove any need
-;; to (require 'org) so early in the configuration.
-(add-to-list 'load-path "~/Projects/org-tangle-config")
-(require 'org-tangle-config)
-(org-tangle-config-before-load "~/.config/emacs/config.org")
+;; Bootstrap Quelpa so that I may use `org-tangle-config'
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents
+     "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+(setq quelpa-update-melpa-p nil) ; Please don't attempt to replace package.el
+(require 'quelpa)
 
 ;;; Use a custom variables file. Load these variables first.
 (setq custom-file (locate-user-emacs-file "custom-vars.el"))
+
+;; Load config.el if it exists. If it does not, tangle the configuration file
+;; and prompt the user to restart. By using this method, we remove any need
+;; to (require 'org) so early in the configuration.
+(quelpa '(org-tangle-config
+          :fetcher github :repo "trev-dev/org-tangle-config.el"))
+(org-tangle-config-before-load "~/.config/emacs/config.org")
 (load custom-file 'noerror 'nomessage)
+
 (provide 'emacs)
 ;;; init.el ends here
